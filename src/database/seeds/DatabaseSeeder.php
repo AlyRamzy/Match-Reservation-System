536caa802bff9fbb7fe2468c3122062823f55f42
AlyRamzy <alyhassan62@yahoo.com>
2021-01-31 20:56:39 +0200

    Fix Seeds:

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            AdminSeeder::class,
+            UserSeeder::class,
             TeamsSeeder::class,
             StadiumSeeder::class,
             MatchSeeder::class,
